28d7fd6d4da43375a62c094e460e8decdb35d75a
Jeff15113 <itsmejeffery@gmail.com>
2019-01-10 18:19:05 -0500

    added check if task is not blank

--- src/App.js
+++ src/App.js
@@ -26,7 +26,8 @@ class App extends React.Component {
   handleAddTask = (e) => {
     e.preventDefault();
     let tempArray = this.state.taskData;
-    tempArray.push({ task: this.state.task, id: Date.now(), completed: false });
+    if (this.state.task != "")
+      tempArray.push({ task: this.state.task, id: Date.now(), completed: false });
     this.setState({ taskData: tempArray, task: '' });
 
   }
